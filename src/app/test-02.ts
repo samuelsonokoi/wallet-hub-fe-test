8c75b51c9221d16adb0d5a65e744657cf66b7d1b
Samuelson Okoi <samuelsonokoi@gmail.com>
2022-10-05 11:22:07 +0100

    completed test two

--- src/app/test-02.ts
+++ src/app/test-02.ts
@@ -3,37 +3,94 @@
  * * Bind `field` of [textfield] component to its text input
  * * Pass value of `field` from [textfield] component to [title] property of component [ng-app]
  */
-import { Component, NgModule } from '@angular/core';
+import {
+	Component,
+	EventEmitter,
+	Input,
+	NgModule,
+	Output,
+} from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
 @Component({
 	selector: 'textfield',
-	template: '<input type="text" value="" />',
+	template: `
+		<input
+			type="text"
+			[(ngModel)]="field"
+			(ngModelChange)="emitNewValue($event)"
+		/>
+	`,
 })
+/**
+ * TextField Component
+ * title {string} - Passed from parent.
+ * field {string}
+ * fieldChange {EventEmitter}
+ * emitNewValue {function} - Assign and emit new value to parent.
+ */
 export class TextField {
-	field = '';
+	@Input() field = '';
+	@Output() fieldChange = new EventEmitter<string>();
+
+	emitNewValue(newValue) {
+		this.field = newValue;
+		this.fieldChange.emit(this.field);
+	}
 }
 
 @Component({
 	selector: 'child-component',
 	template: `<h2>
 		Title:
-		<h2><br /><textfield></textfield></h2>
+		<h2>
+			<br /><textfield
+				[(field)]="title"
+				(fieldChange)="emitNewTitle($event)"
+			></textfield>
+		</h2>
 	</h2>`,
 })
-export class ChildComponent {}
+/**
+ * Child Component
+ * title {string} - Passed from parent.
+ * titleChange {EventEmitter}
+ * emitNewTitle {function} - Assign and emit new value to parent.
+ */
+export class ChildComponent {
+	@Input() title: string;
+	@Output() titleChange = new EventEmitter<string>();
+
+	emitNewTitle(newValue) {
+		this.title = newValue;
+		this.titleChange.emit(this.title);
+	}
+}
 
 @Component({
 	selector: 'ng-app',
 	template: `<div>
-		<child-component></child-component> <br />
+		<child-component
+			[(title)]="title"
+			(titleChange)="updateTitle($event)"
+		></child-component>
+		<br />
 		Title is {{ title }}
 	</div>`,
 })
+/**
+ * Test02 Component
+ * title {string}
+ * updateTitle {function} - Update new title value gotten from child.
+ */
 export class Test02Component {
 	title: string = '';
+
+	updateTitle(newValue) {
+		this.title = newValue;
+	}
 }
 
 @NgModule({
